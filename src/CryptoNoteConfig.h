b3844a60af93299f3ae705aa431c8e764062735c
ScamCoin-Core <40339806+ScamCoin-Core@users.noreply.github.com>
2018-06-24 02:34:27 +0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -146,6 +146,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 const std::initializer_list<const char*> SEED_NODES = {
    "108.61.57.73:11500"
+   "108.61.57.81:11500"
 };
 
 struct CheckpointData {
